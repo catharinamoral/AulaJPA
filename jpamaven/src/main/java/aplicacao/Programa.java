e50111558ff41158942d3df7aca9af86d24f4a1a
C. Moral Hermacula <c.moral.hermacula@avanade.com>
2020-06-15 10:42:03 -0300

    Implementar a persistencia

--- jpamaven/src/main/java/aplicacao/Programa.java
+++ jpamaven/src/main/java/aplicacao/Programa.java
@@ -1,19 +1,31 @@
 package aplicacao;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 import dominio.Pessoa;
 
 public class Programa {
 
 	public static void main(String[] args) {
 
-		Pessoa p1 = new Pessoa(1, "Carlos da Silva", "carlos@gmail.com");
-		Pessoa p2 = new Pessoa(2, "Alberto da Silva", "alberto@gmail.com");
-		Pessoa p3 = new Pessoa(3, "Julia da Silva", "julia@gmail.com");
-
-		System.out.println(p1);
-		System.out.println(p2);
-		System.out.println(p3);
-
+		Pessoa p1 = new Pessoa(null, "Carlos da Silva", "carlos@gmail.com");
+		Pessoa p2 = new Pessoa(null, "Alberto da Silva", "alberto@gmail.com");
+		Pessoa p3 = new Pessoa(null, "Julia da Silva", "julia@gmail.com");
+		
+		//instancia o EntityManagerFactory com as configurações do persistence.xml
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
+		//conexao com banco de dados e contexto de persistencia implementado
+		EntityManager em = emf.createEntityManager();
+		
+		em.getTransaction().begin(); //inicia transação com o banco de dados
+		em.persist(p1);
+		em.persist(p2);
+		em.persist(p3);
+		em.getTransaction().commit(); //confirma as alterações realizadas
+		System.out.println("pronto!");
+		
 	}
 
 }
